55dd27181c49b66f918dd6b9af80265c7848c3be
mateusz713 <mateusz713@gmail.com>
2014-11-06 09:29:05 +0100

    poranek dzien drugi...

--- cw3_zad2.py
+++ cw3_zad2.py
@@ -3,3 +3,36 @@ Created on 05-11-2014
 
 @author: Latino
 '''
+plik=open("menu_restauracji.txt","r")
+
+menu={}
+wyrazy = plik.read()
+wyrazy = str(wyrazy)
+wyrazy = wyrazy.replace("zl", " - ")
+wyrazy = wyrazy.replace("\n", "")
+wyrazy = wyrazy.split(" - ")
+a=len(wyrazy)
+del wyrazy[a-1]
+aa=0
+
+while aa<a-2:
+    menu[wyrazy[aa]]=float(wyrazy[aa+1])
+    aa=aa+2   
+    
+def kasa(zamowienie):
+    cena = 0
+    napiwek = 0
+    for i in zamowienie:
+        if i in menu:
+            cena+=menu[i]
+        napiwek = 0.2*cena
+    print "Naleznosc: "+str(cena)+"zl     napiwek: "+str(napiwek)+"zl"
+    return 0
+
+
+kasa(["kapusta","zupa","czekolada"])
+kasa(["kurczak","zupa"])
+
+#print menu
+
+plik.close()
