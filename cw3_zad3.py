0cafdefd61b18ef835a2d429b5a5761b885080f6
mateusz713 <mateusz713@gmail.com>
2014-11-17 11:35:40 +0100

    poranek dzien trudno zliczyc...

--- cw3_zad3.py
+++ cw3_zad3.py
@@ -3,3 +3,39 @@ Created on 05-11-2014
 
 @author: Latino
 '''
+import math
+
+class Zespolona:
+    def __init__(self,r,u):
+        self.r=r
+        self.u=u
+    def get_r(self):
+        return self.__r
+    def set_r(self,r):
+        self.__r=r
+    def get_u(self):
+        return self.__u
+    def set_u(self,u):
+        self.__u=u
+    def __str__(self):
+        return "zespolona = "+str(self.r)+" + "+str(self.u)+"i"
+    
+    def modul2(self): 
+        return "modul liczby = "+str(math.sqrt(self.r**2+self.u**2))
+    
+    def dodaj_zespolona2(self,z2): 
+        nowar=self.r+z2.r;
+        nowau=self.u+z2.u;
+        return "wynik dodawania = "+str(nowar)+" + "+str(nowau)+"i"
+    
+    def odejmij_zespolona2(self,z2): 
+        nowar=self.r-z2.r;
+        nowau=self.u-z2.u;
+        return "wynik odejmowania = "+str(nowar)+" + "+str(nowau)+"i"
+
+z1 = Zespolona(2,3)
+z2 = Zespolona(1,3)
+print str(z1)
+print z1.modul2()
+print z1.dodaj_zespolona2(z2)
+print z1.odejmij_zespolona2(z2)
--- cw3_zad4.py
+++ cw3_zad4.py
@@ -3,3 +3,53 @@ Created on 05-11-2014
 
 @author: Latino
 '''
+class Wojownik:
+    def __init__(self,imie,nazwisko):
+        self.imie=imie
+        self.nazwisko=nazwisko
+    def get_imie(self):
+        return self.__imie
+    def set_imie(self,imie):
+        self.__imie=imie
+    def get_nazwisko(self):
+        return self.__nazwisko
+    def set_nazwisko(self,nazwisko):
+        self.__nazwisko=nazwisko
+    def podaj_imie(self):
+        print "Jestem "+self.imie+" "+self.nazwisko+" wojownik z woli Pana!"
+        
+
+class Rycerz(Wojownik):
+    def __init__(self,imie,nazwisko,status):
+        Wojownik.__init__(self, imie, nazwisko)
+        self.status=status
+    def podaj_imie(self):
+        print "Jam jest "+self.status+" "+self.imie+" "+self.nazwisko+" rycerz z woli Pana!"
+    def bij_wroga(self):
+        print "Bic psubraty!"
+        
+class Sad:
+    def podaj_wyrok(self):
+        print "skazuje Cie niegodziwcze!" 
+        
+class Paladyn(Rycerz, Sad):
+    pass
+        
+class Lucznik(Wojownik):
+    def __init__(self,imie,nazwisko):
+        Wojownik.__init__(self,imie,nazwisko)
+    def podaj_imie(self):
+        print "Jestem "+self.imie+" "+self.nazwisko+" lucznik"
+    def przygotuj_orez(self):
+        print "Naciagac cieciwe!" 
+    def bij_wroga(self):
+        print "Strzelam!"
+
+
+zbyszko = Wojownik("Zbyszek", "Silnoreki")
+zbyszko.podaj_imie()
+zbyszko2 = Rycerz("Zbych", "Szybkonogi","Szlachcic")
+zbyszko2.podaj_imie()
+zbyszko3 = Paladyn("Zbychu", "Wielki","Paladyn")
+zbyszko3.podaj_imie()
+zbyszko3.podaj_wyrok()
